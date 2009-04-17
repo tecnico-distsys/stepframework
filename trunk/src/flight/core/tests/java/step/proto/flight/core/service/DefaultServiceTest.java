f1c1db8f16abdcfa2a892837d60a5b0127d9a071
mflpar <mflpar@7e0a3e13-f94c-0410-a380-de2e0ff8c08b>
2009-04-17 16:21:28 +0000

    Delete flight and mediator to prepare for massive rename

--- trunk/src/flight/core/tests/java/step/proto/flight/core/service/DefaultServiceTest.java
+++
@@ -1,7 +0,0 @@
-package step.proto.flight.core.service;
-public class DefaultServiceTest extends step.framework.service.ServiceTest {
-    @Override
-    protected String getSetupDataSetName() {
-	return "/flight-core.xml";
-    }
-}
--- trunk/src/flight/view/config/jax-b/readme.txt
+++
@@ -1,4 +0,0 @@
-place jax-b custom binding files in this directory
-place xsd files in xml sources directory
-
-this placeholder file also assures this required directory is properly handled by CVS
