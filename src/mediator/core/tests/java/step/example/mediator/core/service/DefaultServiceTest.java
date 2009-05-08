e0280d2f657a732d99190a51f90519b597b9eef7
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2009-05-08 14:27:17 +0000

    Delete STEP 1.1 flight and mediator applications before switching to new directory layout (Eclipse friendly)

--- src/mediator/core/tests/java/step/example/mediator/core/service/DefaultServiceTest.java
+++
@@ -1,8 +0,0 @@
-package step.example.mediator.core.service;
-
-public class DefaultServiceTest extends step.framework.service.ServiceTest {
-	@Override
-	protected String getSetupDataSetName() {
-		return "/mediator-core.xml";
-	}
-}
