947da7ea5e7420446dd50d5833dab981c06fe007
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2010-03-19 15:36:18 +0000

    Reverted WSIManager to 1.3 version. Automated assertions of extensions-testbed

--- src_test/extensions-testbed/hello-ws/src/hello/ws/HelloWebServiceImpl.java
+++ src_test/extensions-testbed/hello-ws/src/hello/ws/HelloWebServiceImpl.java
@@ -1,5 +1,8 @@
 package hello.ws;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import step.framework.config.Config;
 import step.framework.config.ConfigUtil;
 
@@ -13,8 +16,14 @@ import hello.wsdl.ServiceError_Exception;
 @javax.jws.WebService (endpointInterface="hello.wsdl.HelloPortType")
 public class HelloWebServiceImpl implements HelloPortType {
 
+    /** logging */
+    private static Log log = LogFactory.getLog(HelloWebServiceImpl.class);
+    
     public String sayHello(String name) throws ServiceError_Exception {
         try	{
+            log.trace("Starting sayHello on server");
+            log.trace("name = " + name);
+            
             boolean throwError = ConfigUtil.recognizeAsTrue(Config.getInstance().getInitParameter("hello.ws.ThrowServiceError"));
             if(throwError)
                 throw new MissingNameException("Name is missing (forced by hello.ws.ThrowServiceError property for testing)", null);
@@ -22,10 +31,18 @@ public class HelloWebServiceImpl implements HelloPortType {
             if(name == null || name.trim().length() == 0)
                 throw new MissingNameException("Name is missing", null);
 
-            return "Hello " + name;
+            String returnValue = "Hello " + name;
+            log.trace("returnValue = " + returnValue);
+            return returnValue;
 
         } catch (Exception e) {
+            log.trace("Caught exception");
+            log.error(e);
+            log.trace("Rethrowing inside a ServiceError_Exception");
             throw new ServiceError_Exception(e.getMessage(), new ServiceError(), e);
+
+        } finally {
+            log.trace("Finally exiting sayHello on server");
         }
 
     }
--- src_test/extensions-testbed/helloclient-web/src/hello/web/SayHelloAction.java
+++ src_test/extensions-testbed/helloclient-web/src/hello/web/SayHelloAction.java
@@ -1,16 +1,24 @@
 package hello.web;
 
-import hello.ws.client.service.SayHelloService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 
-public class SayHelloAction extends HelloAction
-{
+import hello.ws.client.service.SayHelloService;
+
+
+public class SayHelloAction extends HelloAction {
+
+    /** logging */
+    private static Log log = LogFactory.getLog(SayHelloAction.class);
+
+    /* members */
 	private String name;
 	private String message;
-
+    
 	public String getName() {
 		return name;
 	}
@@ -35,6 +43,8 @@ public class SayHelloAction extends HelloAction
 
 			setMessage(greeting);
 		} catch(Exception e) {
+            log.trace("sayHello caught exception");
+            log.error(e);
 			setMessage(e.getMessage());
 		}
 
@@ -44,10 +54,26 @@ public class SayHelloAction extends HelloAction
 
     /* For console testing */
     public static void main(String[] args) throws Exception {
-        SayHelloAction action = new SayHelloAction();
-        action.setName(args[0]);
-        action.sayHello();
-        System.out.println(action.getMessage());
+        try {
+            log.trace("Starting main");
+            log.trace(args);
+
+            SayHelloAction action = new SayHelloAction();
+            action.setName(args[0]);
+
+            log.trace("Invoking sayHello()");
+            action.sayHello();
+
+            log.trace("Action message: " + action.getMessage());
+            System.out.println(action.getMessage());
+
+        } catch(Throwable t) {
+            log.trace("Caught throwable");
+            log.error(t);
+
+        } finally {
+            log.trace("Finally exiting main");
+        }
     }
     
 }
