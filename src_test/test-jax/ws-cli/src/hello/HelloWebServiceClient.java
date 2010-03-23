c84b1cff6b90ebdb8ea04fba0b1fe065add9e415
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2010-03-23 23:57:54 +0000

    Added updated version of src_test containing projects to test JAX-WS engine

---
+++ src_test/test-jax/ws-cli/src/hello/HelloWebServiceClient.java
@@ -0,0 +1,33 @@
+package hello;
+
+import javax.xml.ws.BindingProvider;
+
+import hello.wsdl.*;
+import java.util.*;
+
+public class HelloWebServiceClient {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Web Service client starting...");
+
+        System.out.println("Creating stub and configuring it...");
+
+        // create stub
+        HelloService service = new HelloService();
+        HelloPortType port = service.getHelloPort();
+        BindingProvider bindingProvider = (BindingProvider) port;
+
+        // set endpoint address
+        String endpointAddress = "http://localhost:8080/ws/endpoint";
+        System.out.println("Setting endpoint address to: " + endpointAddress);
+        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
+
+        // invoke web service and print result
+        System.out.println("Invoking Web Service...");
+        String sayHelloResult = port.sayHello(null);
+        System.out.println("The result is: " + sayHelloResult);
+
+    }
+
+}
