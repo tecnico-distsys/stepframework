6ffeaf913716eb80b95e4e7e1bc666d7f8c576c4
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2010-04-09 10:41:55 +0000

    Added groovy tests for create reservation (both single and multiple)

--- src/TestWS.groovy
+++ src/TestWS.groovy
@@ -20,6 +20,8 @@ StubUtil.setPortEndpointAddress(port, "http://localhost:8080/flight-ws/endpoint"
 //  Search flights
 //
 
+def flightToBook = null;
+
 // fill in request
 def sfIn = Class.forName("org.tripplanner.flight.view.SearchFlightsInput").newInstance();
 sfIn.depart = "Lisbon";
@@ -29,13 +31,17 @@ try {
     // invoke
     def sfOut = port.searchFlights(sfIn);
     // print results
-    println "Response includes " + sfOut.flights.size() + " flights";
+    println "Response:";
+    println sfOut.flights.size() + " flights";
     sfOut.flights.each {
-        println it.number;
-        println it.date;
-        println it.depart;
-        println it.arrive;
-        println it.price.currencyCode + " " + it.price.value;
+        print "Flight " + it.number + " on " + it.date;
+        print " from " + it.depart + " to " + it.arrive;
+        println " costing " + it.price.code + " " + it.price.value;
+    }
+
+    if(sfOut.flights.size() > 0) {
+        flightToBook = sfOut.flights.get(0);
+        println "Picked flight " + flightToBook.number + " to book reservations";
     }
 
 } catch(e) {
@@ -47,8 +53,58 @@ try {
 //  Create single reservation
 //
 
+// fill in request
+def csrIn = Class.forName("org.tripplanner.flight.view.CreateSingleReservationInput").newInstance();
+csrIn.flightNumber = flightToBook.number;
+def singlePassenger = Class.forName("org.tripplanner.flight.view.PassengerView").newInstance();
+singlePassenger.id = "123";
+singlePassenger.id = "Alpha";
+csrIn.passenger = singlePassenger;
+
+try {
+    // invoke
+    def csrOut = port.createSingleReservation(csrIn);
+    // print results
+    println "Response:";
+    println "Reservation code " + csrOut.reservation.code + " for flight " + csrOut.reservation.flightNumber;
+
+} catch(e) {
+    println("Caught exception " + e);
+}
+
 
 //
 //  Create multiple reservations
 //
+
+// fill in request
+def cmrIn = Class.forName("org.tripplanner.flight.view.CreateMultipleReservationsInput").newInstance();
+cmrIn.flightNumber = flightToBook.number;
+
+def passengerList = cmrIn.passengers;
+
+def passenger1 = Class.forName("org.tripplanner.flight.view.PassengerView").newInstance();
+passenger1.id = "1010";
+passenger1.id = "Number One";
+passengerList.add(passenger1);
+
+def passenger2 = Class.forName("org.tripplanner.flight.view.PassengerView").newInstance();
+passenger2.id = "2020";
+passenger2.id = "Number Two";
+passengerList.add(passenger2);
+
+try {
+    // invoke
+    def cmrOut = port.createMultipleReservations(cmrIn);
+
+    // print results
+    println "Response:";
+    println cmrOut.reservations.size() + " reservations";
+    cmrOut.reservations.each {
+        println "Reservation code " + it.code + " for flight " + it.flightNumber;
+    }
+
+} catch(e) {
+    println("Caught exception " + e);
+}
 
