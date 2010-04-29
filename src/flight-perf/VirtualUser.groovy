8e6d65a9db5008157a7415d9c6f4e374416a1d49
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2010-04-29 15:16:09 +0000

    Completed WorkloadGenerator and VirtualUser using command framework

--- src/flight-perf/VirtualUser.groovy
+++ src/flight-perf/VirtualUser.groovy
@@ -1,67 +1,155 @@
 /**
- *  Groovy script to execute simulated user requests.
+ *  Groovy command to execute simulated user requests using a provided workload.
  */
 
+import org.apache.commons.cli.*;
+import groovy.sql.Sql;
 
-//
-//  Initialization
-//
+public class VirtualUser extends ByYourCommand {
 
-// add library locations to class loader
-def root = "..";
-ClassLoaderHelper.addFile(root + "/framework/dist/stepframework.jar");
-ClassLoaderHelper.addFile(root + "/flight-ws-cli/dist/flight-ws-cli.jar");
+    // --- static ---
 
-FileInputStream fis = new FileInputStream("build/requests.obj");
-ObjectInputStream ois = new ObjectInputStream(fis);
-
-// create Web Service stub
-def service = Class.forName("org.tripplanner.flight.wsdl.FlightService").newInstance();
-def port = service.getFlightPort();
-
-// set endpoint address
-def StubUtil = Class.forName("step.framework.ws.StubUtil");
-StubUtil.setPortEndpointAddress(port, "http://localhost:8080/flight-ws/endpoint");
+    //
+    //  Command line execution
+    //
+    public static void main(String[] args) {
+        VirtualUser instance = new VirtualUser();
+        if (instance.handleCommandLineArgs(args)) {
+            instance.run();
+        }
+    }
 
 
-//
-//  Simulate requests
-//
+    //
+    //  1-time setup
+    //
+    static setupFlag = true;
 
-def eof = false;
-while (!eof) {
-    try {
-        def operation = ois.readObject();
+    static void setup() {
+        if (setupFlag) {
+            // load environment properties
+            def env = System.getenv();
+            // load libraries
+            //ClassLoaderHelper.addJarDir(env["STEP_HOME"] + "/lib");
+            ClassLoaderHelper.addFile("../framework/dist/stepframework.jar");
+            ClassLoaderHelper.addFile("../flight-ws-cli/dist/flight-ws-cli.jar");
 
-        if ("THINK".equals(operation)) {
-            def thinkSeconds = ois.readObject();
-            if(thinkSeconds > 0) {
-                println "Think " + thinkSeconds + " seconds";
-                Thread.sleep(thinkSeconds * 1000);
+            // clear flag
+            setupFlag = false;
+        }
+    }
+
+
+    // --- instance ---
+
+    File inputFile;
+
+    ObjectInputStream ois;
+    String endpoint;
+
+
+    @Override protected Options createCommandLineOptions() {
+        Options options = super.createCommandLineOptions();
+
+        options.addOption(CommandHelper.buildInputOption());
+        options.addOption(CommandHelper.buildOutputOption());
+
+        Option endpointOption = new Option("e", "endpoint",
+            /* hasArg */ true, "Web Service endpoint address");
+        endpointOption.setArgName("url");
+        endpointOption.setArgs(1);
+        options.addOption(endpointOption);
+
+        return options;
+    }
+
+    @Override protected boolean cmdInit() {
+        if (!super.cmdInit()) return false;
+
+        if (inputFile == null) {
+            inputFile = CommandHelper.initFile(settings[CommandHelper.INPUT_LOPT], null);
+            if (inputFile == null) {
+                err.println("Input file setting is missing!");
+                return false;
             }
-
-        } else if ("SEARCH_FLIGHTS".equals(operation)) {
-            def sfIn = ois.readObject();
-            println "Search flights from " + sfIn.depart + " to " + sfIn.arrive;
-            port.searchFlights(sfIn);
-
-        } else if ("CREATE_SINGLE_RESERVATION".equals(operation)) {
-            def csrIn = ois.readObject();
-            println "Create single reservation for flight " + csrIn.flightNumber;
-            port.createSingleReservation(csrIn);
-
-        } else if ("CREATE_MULTIPLE_RESERVATIONS".equals(operation)) {
-            def cmrIn = ois.readObject();
-            println "Create " + cmrIn.passengers.size() + " reservations for flight " + cmrIn.flightNumber;
-            port.createMultipleReservations(cmrIn);
-
-        } else {
-            throw new RuntimeException("Unknown operation " + operation + ".");
         }
 
-    } catch(EOFException eofe) {
-        eof = true;
-    }
-}
+        if (ois == null) {
+            FileInputStream fis = new FileInputStream(inputFile);
+            ois = new ObjectInputStream(fis);
+        }
 
-println "Done!";
+        if (endpoint == null) {
+            endpoint = settings["endpoint"];
+            if (endpoint == null) {
+                endpoint = "http://localhost:8080/flight-ws/endpoint";
+            }
+        }
+
+        return true
+    }
+
+    @Override protected void cmdRun() {
+        VirtualUser.setup();
+
+        err.println("Running " + this.class.simpleName);
+        err.printf("input file %s, endpoint %s",
+            inputFile, endpoint);
+        err.println();
+
+        // create Web Service stub
+        def service = Class.forName("org.tripplanner.flight.wsdl.FlightService").newInstance();
+        def port = service.getFlightPort();
+
+        // set endpoint address
+        def StubUtil = Class.forName("step.framework.ws.StubUtil");
+        StubUtil.setPortEndpointAddress(port, endpoint);
+
+        //
+        //  Send requests
+        //
+
+        def eof = false;
+        while (!eof) {
+            try {
+                def operation = ois.readObject();
+
+                if ("THINK".equals(operation)) {
+                    def thinkSeconds = ois.readObject();
+                    if(thinkSeconds > 0) {
+                        err.println("Think " + thinkSeconds + " seconds");
+                        Thread.sleep(thinkSeconds * 1000);
+                    }
+
+                } else if ("SEARCH_FLIGHTS".equals(operation)) {
+                    def sfIn = ois.readObject();
+                    err.println("Search flights from " + sfIn.depart + " to " + sfIn.arrive);
+                    port.searchFlights(sfIn);
+
+                } else if ("CREATE_SINGLE_RESERVATION".equals(operation)) {
+                    def csrIn = ois.readObject();
+                    err.println("Create single reservation for flight " + csrIn.flightNumber);
+                    port.createSingleReservation(csrIn);
+
+                } else if ("CREATE_MULTIPLE_RESERVATIONS".equals(operation)) {
+                    def cmrIn = ois.readObject();
+                    err.println("Create " + cmrIn.passengers.size() + " reservations for flight " + cmrIn.flightNumber);
+                    port.createMultipleReservations(cmrIn);
+
+                } else {
+                    throw new RuntimeException("Unknown operation " + operation + ".");
+                }
+
+            } catch(EOFException eofe) {
+                eof = true;
+            } catch(Exception e) {
+                err.println("Caught " + e);
+                err.println("Proceeding");
+            }
+        }
+
+        err.println("Done!");
+
+    }
+
+}
--- src/flight-perf/WorkloadGenerator.groovy
+++ src/flight-perf/WorkloadGenerator.groovy
@@ -1,132 +1,265 @@
 /**
- *  Groovy script to generate Web Service requests
+ *  Groovy command to generate Web Service requests
  */
 
-//
-//  Constants
-//
+import org.apache.commons.cli.*;
+import groovy.sql.Sql;
 
-final def MAX_THINK = 5;     // seconds
-final def MAX_GROUP = 6;     // multiple reservation group size
-final def MAX_PASSENGER_ID = 100000;
+public class WorkloadGenerator extends DBCommand {
 
-
-//
-//  Initialization
-//
-
-// add library locations to class loader
-def root = "..";
-ClassLoaderHelper.addFile(root + "/framework/dist/stepframework.jar");
-ClassLoaderHelper.addFile(root + "/flight-ws-cli/dist/flight-ws-cli.jar");
-
-// random
-def random = new Random(); // TODO seed
-
-NameGenerator nameGenerator = new NameGenerator("data/names.txt", "data/surnames.txt", random);
-
-// object output stream
-FileOutputStream fos = new FileOutputStream("build/requests.obj");
-ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-// database
-def sql = DBHelper.init();
-
-// operation identifier string
-def operation;
-
-
-def count = 0;
-while(count < 10) {
-    count++;
+    // --- static ---
 
     //
-    //  generate search flights request
+    //  Command line execution
     //
-    operation = "SEARCH_FLIGHTS";
-
-    def flight = DBHelper.pickRandomFlight(sql, random);
-
-    def depart = DBHelper.getAirport(sql, flight.origin_id).city;
-    def arrive = DBHelper.getAirport(sql, flight.destination_id).city;
-
-    def sfIn = Class.forName("org.tripplanner.flight.view.SearchFlightsInput").newInstance();
-    sfIn.depart = depart;
-    sfIn.arrive = arrive;
-
-    oos.writeObject(operation);
-    oos.writeObject(sfIn);
-
-
-    //
-    //  generate think time
-    //
-    operation = "THINK";
-
-    oos.writeObject(operation);
-    oos.writeObject(random.nextInt(MAX_THINK));
-
-
-    //
-    //  generate create reservations
-    //
-    def groupSize = random.nextInt(MAX_GROUP);
-
-    if(groupSize == 0) {
-        // no reservation
-
-    } else if(groupSize == 1) {
-        //
-        //  generate create single reservation
-        //
-        operation = "CREATE_SINGLE_RESERVATION";
-
-        def csrIn = Class.forName("org.tripplanner.flight.view.CreateSingleReservationInput").newInstance();
-        csrIn.flightNumber = flight.number;
-        def singlePassenger = Class.forName("org.tripplanner.flight.view.PassengerView").newInstance();
-        singlePassenger.id = random.nextInt(MAX_PASSENGER_ID);
-        singlePassenger.name = nameGenerator.nextName();
-        csrIn.passenger = singlePassenger;
-
-        oos.writeObject(operation);
-        oos.writeObject(csrIn);
-
-    } else {
-        //
-        //  generate multiple reservations
-        //
-        operation = "CREATE_MULTIPLE_RESERVATIONS";
-
-        def cmrIn = Class.forName("org.tripplanner.flight.view.CreateMultipleReservationsInput").newInstance();
-        cmrIn.flightNumber = flight.number;
-
-        def passengerList = cmrIn.passengers;
-
-        for(int i = 0; i < groupSize; i++) {
-            def groupPassenger = Class.forName("org.tripplanner.flight.view.PassengerView").newInstance();
-            groupPassenger.id = random.nextInt(MAX_PASSENGER_ID);
-            groupPassenger.name = nameGenerator.nextName();
-            passengerList.add(groupPassenger);
+    public static void main(String[] args) {
+        WorkloadGenerator instance = new WorkloadGenerator();
+        if (instance.handleCommandLineArgs(args)) {
+            instance.run();
         }
-
-        oos.writeObject(operation);
-        oos.writeObject(cmrIn);
-
     }
 
 
     //
-    //  generate think time
+    //  1-time setup
     //
-    operation = "THINK";
+    static setupFlag = true;
 
-    oos.writeObject(operation);
-    oos.writeObject(random.nextInt(MAX_THINK));
+    static void setup() {
+        if (setupFlag) {
+            // load environment properties
+            def env = System.getenv();
+            // load libraries
+            //ClassLoaderHelper.addJarDir(env["STEP_HOME"] + "/lib");
+            ClassLoaderHelper.addFile("../framework/dist/stepframework.jar");
+            ClassLoaderHelper.addFile("../flight-ws-cli/dist/flight-ws-cli.jar");
+
+            // clear flag
+            setupFlag = false;
+        }
+    }
+
+
+    // --- instance ---
+
+    final def MAX_PASSENGER_ID = Integer.MAX_VALUE;
+
+    Long seed;
+    Random random;
+
+    Integer number;
+
+    Integer maxGroup;
+    Integer maxThinkTime;
+
+    File namesFile;
+    File surnamesFile;
+
+    File outputFile;
+    ObjectOutputStream oos;
+
+
+    @Override protected Options createCommandLineOptions() {
+        Options options = super.createCommandLineOptions();
+
+        options.addOption(CommandHelper.buildSeedOption());
+        options.addOption(CommandHelper.buildNrOption());
+        options.addOption(CommandHelper.buildMaxGroupOption());
+
+        Option mttOption = new Option("Mtt", "maxthinktime",
+            /* hasArg */ true, "Maximum think time between requests");
+        mttOption.setArgName("seconds");
+        mttOption.setArgs(1);
+        options.addOption(mttOption);
+
+        Option nOption = new Option("nms", "names",
+            /* hasArg */ true, "Names file");
+        nOption.setArgName("data file");
+        nOption.setArgs(1);
+        options.addOption(nOption);
+
+        Option snOption = new Option("snms", "surnames",
+            /* hasArg */ true, "Surnames file");
+        snOption.setArgName("data file");
+        snOption.setArgs(1);
+        options.addOption(snOption);
+
+        options.addOption(CommandHelper.buildOutputOption());
+
+        return options;
+    }
+
+    @Override protected boolean cmdInit() {
+        if (!super.cmdInit()) return false;
+
+        if (seed == null) {
+            seed = CommandHelper.initLong(settings[CommandHelper.SEED_LOPT], null);
+        }
+
+        if (random == null) {
+            random = CommandHelper.initRandom(seed);
+        }
+
+        if (number == null) {
+            number = CommandHelper.initInteger(settings[CommandHelper.NR_LOPT], null);
+            if (number == null || number <= 0) {
+                err.println("Number of request sessions is missing or does not make sense!");
+                return false;
+            }
+        }
+
+        if (maxGroup == null) {
+            maxGroup = CommandHelper.initInteger(settings[CommandHelper.MAX_GROUP_LOPT], 10);
+        }
+
+        if (maxThinkTime == null) {
+            maxThinkTime = CommandHelper.initInteger(settings["maxthinktime"], 5);  // seconds
+        }
+
+        if (namesFile == null) {
+            namesFile = CommandHelper.initFile(settings["names"], null);
+            if (namesFile == null || !namesFile.exists()) {
+                err.println("Names file is missing or does not exist!");
+                return false;
+            }
+        }
+
+        if (surnamesFile == null) {
+            surnamesFile = CommandHelper.initFile(settings["surnames"], null);
+            if (surnamesFile == null || !surnamesFile.exists()) {
+                err.println("Surnames file is missing or does not exist!");
+                return false;
+            }
+        }
+
+        if (outputFile == null) {
+            outputFile = CommandHelper.initFile(settings[CommandHelper.OUTPUT_LOPT], null);
+            if (outputFile == null) {
+                err.println("Output file setting is missing!");
+                return false;
+            }
+        }
+
+        if (oos == null) {
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            oos = new ObjectOutputStream(fos);
+        }
+
+        return true
+    }
+
+
+    @Override protected void dbRun() {
+        WorkloadGenerator.setup();
+
+        err.println("Running " + this.class.simpleName);
+        err.printf("output %d request sessions to %s, name data files %s %s, max group %d, max think time %d",
+            number, outputFile, namesFile, surnamesFile, maxGroup, maxThinkTime);
+        if(seed != null) err.printf(", random seed %d", seed);
+        err.println();
+
+        // name generator
+        NameGenerator nameGenerator = new NameGenerator(namesFile.path, surnamesFile.path, random);
+
+        // operation identifier string
+        def operation;
+
+        def count = 0;
+        while(count < number) {
+            count++;
+
+            //
+            //  generate search flights request
+            //
+            operation = "SEARCH_FLIGHTS";
+
+            def flight = FlightDBHelper.pickRandomFlight(sql, random);
+
+            def depart = FlightDBHelper.getAirport(sql, flight.origin_id).city;
+            def arrive = FlightDBHelper.getAirport(sql, flight.destination_id).city;
+
+            def sfIn = Class.forName("org.tripplanner.flight.view.SearchFlightsInput").newInstance();
+            sfIn.depart = depart;
+            sfIn.arrive = arrive;
+
+            oos.writeObject(operation);
+            oos.writeObject(sfIn);
+
+
+            //
+            //  generate think time
+            //
+            operation = "THINK";
+
+            oos.writeObject(operation);
+            oos.writeObject(random.nextInt(maxThinkTime));
+
+
+            //
+            //  generate create reservations
+            //
+            def groupSize = random.nextInt(maxGroup);
+
+            if(groupSize == 0) {
+                // no reservation
+
+            } else if(groupSize == 1) {
+                //
+                //  generate create single reservation
+                //
+                operation = "CREATE_SINGLE_RESERVATION";
+
+                def csrIn = Class.forName("org.tripplanner.flight.view.CreateSingleReservationInput").newInstance();
+                csrIn.flightNumber = flight.number;
+                def singlePassenger = Class.forName("org.tripplanner.flight.view.PassengerView").newInstance();
+                singlePassenger.id = random.nextInt(MAX_PASSENGER_ID);
+                singlePassenger.name = nameGenerator.nextName();
+                csrIn.passenger = singlePassenger;
+
+                oos.writeObject(operation);
+                oos.writeObject(csrIn);
+
+            } else {
+                //
+                //  generate multiple reservations
+                //
+                operation = "CREATE_MULTIPLE_RESERVATIONS";
+
+                def cmrIn = Class.forName("org.tripplanner.flight.view.CreateMultipleReservationsInput").newInstance();
+                cmrIn.flightNumber = flight.number;
+
+                def passengerList = cmrIn.passengers;
+
+                for(int i = 0; i < groupSize; i++) {
+                    def groupPassenger = Class.forName("org.tripplanner.flight.view.PassengerView").newInstance();
+                    groupPassenger.id = random.nextInt(MAX_PASSENGER_ID);
+                    groupPassenger.name = nameGenerator.nextName();
+                    passengerList.add(groupPassenger);
+                }
+
+                oos.writeObject(operation);
+                oos.writeObject(cmrIn);
+
+            }
+
+
+            //
+            //  generate think time
+            //
+            operation = "THINK";
+
+            oos.writeObject(operation);
+            oos.writeObject(random.nextInt(maxThinkTime));
+
+        }
+
+
+        //
+        //  Finish
+        //
+        oos.close();
+
+
+    }
 
 }
-
-
-//
-//  Finish
-//
-oos.close();
