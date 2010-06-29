ad8a750e7aa2afd621c78908fa87a5f031ccb1ea
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2010-06-29 10:21:19 +0000

    Added stats generation for small, large and xlarge XML sizes; Added XMLSizeZoom gnuplot

---
+++ src/flight-perf/etc/config/StatsLarge.config.groovy
@@ -0,0 +1,16 @@
+/**
+ *  Flight Performance stats instance config
+ *  (syntax http://groovy.codehaus.org/ConfigSlurper)
+ */
+
+perf {
+    flight {
+        stats {
+            instance {
+                runId = "large_"
+                filterId = ""
+                numberSamples = 30
+            }
+        }
+    }
+}
