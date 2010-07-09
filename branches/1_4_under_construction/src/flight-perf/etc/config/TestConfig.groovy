31a6f2541f6b67fa3d55eca9f3abbc010258581b
mflpar <mflpar@7e0a3e13-f94c-0410-a380-de2e0ff8c08b>
2010-07-09 16:58:03 +0000

    Reinserted changed flight perf configurationfiles

---
+++ branches/1_4_under_construction/src/flight-perf/etc/config/TestConfig.groovy
@@ -0,0 +1,102 @@
+/**
+ *  Flight Performance static config
+ *  (syntax http://groovy.codehaus.org/ConfigSlurper)
+ */
+
+perf {
+    flight {
+        sourceCodeDir = ".."
+        databasePropertiesFile = "etc/config/db.properties"
+
+//      ------------------------------------------------------------------------
+        domain {
+            dataDir = "etc/domain-data"
+
+            namesFile = this.perf.flight.domain.dataDir + "/names.txt"
+            surnamesFile = this.perf.flight.domain.dataDir + "/surnames.txt"
+
+            airports {
+                randomSeed = -8354
+                file = this.perf.flight.domain.dataDir + "/airports.csv"
+                maxCost = 150000
+            }
+
+            airplanes {
+                randomSeed = 926783
+                file = this.perf.flight.domain.dataDir + "/fleet-BA.csv"
+                maxCost = 100000
+            }
+
+            flights {
+                randomSeed = 8845868
+                maxCost = 1500
+                profit = 0.20
+                number = 564
+                maxGroup = 100
+            }
+        }
+
+//      ------------------------------------------------------------------------
+        load {
+            instanceDir = "etc/config"
+            instanceFileNameRegex = "(.*?).load"
+
+            outputBaseDir = "../../../flight-perf-testdata/load"
+            outputFileNameFormat = "load-%d.obj"
+        }
+
+//      ------------------------------------------------------------------------
+        run {
+            instanceDir = "etc/config"
+            instanceFileNameRegex = "(.*?).run"
+
+            configFilesBaseDir = "etc/config/files"
+
+            endpoint = "http://localhost:8080/flight-ws/endpoint"
+
+            logFileName = "flight-ws_log.txt"
+
+            perf4JLogFileName = "flight-ws_perfLog.txt"
+            eventMonLogFileNameRegex = "PerfEvent-thr([0-9]+)\\.log"
+            layerMonLogFileNameRegex = "PerfLayer-thr([0-9]+)\\.log"
+
+            outputBaseDir = "../../../flight-perf-testdata/run"
+
+            outputSysInfoFileName = "sysinfo.txt"
+
+            outputFileNameFormat = "run-%d.out"
+
+            outputLogFileNameFormat = "log-%d.txt"
+            outputLogSizeFileNameFormat = "log-%d-size.txt"
+
+            outputPerf4JLogFileNameFormat = "Perf4JLog-%d.txt"
+            outputEventMonLogFileNameFormat = "EventLog-%d.txt"
+            outputLayerMonLogFileNameFormat = "LayerLog-%d.txt"
+        }
+
+//      ------------------------------------------------------------------------
+        stats {
+            instanceDir = "etc/config"
+            instanceFileNameRegex = "(.*?).stats"
+
+            outputBaseDir = "../../../flight-perf-testdata/stats"
+
+            requestsFileNameFormat = "requests-%d.csv"
+
+            samplesFileName = "samples-stats.csv"
+            samplesTextFileName = "samples-stats.txt"
+
+            overallFileName = "stats.csv"
+            overallTextFileName = "stats.txt"
+        }
+
+//      ------------------------------------------------------------------------
+        report {
+
+            plotDir = "etc/plot"
+
+            outputDir = "../../../flight-perf-testdata/report"
+        }
+
+    }
+}
---
+++ branches/1_4_under_construction/src/flight-perf/etc/config/instances/medium.load
@@ -0,0 +1,51 @@
+/**
+ *  Flight Performance load instance config
+ *  (syntax http://groovy.codehaus.org/ConfigSlurper)
+ */
+
+perf {
+    flight {
+        load {
+            instance {
+                id = "medium"
+                numberSessions = 50
+                maxGroup = 100
+                numberSamples = 30
+                randomSeedList = [
+                                      553452, 
+                                     2058242, 
+                                    97585520, 
+                                   171321857, 
+                                  -133984674, 
+                                     1402770, 
+                                   -23707283, 
+                                   186244601, 
+                                   124078905, 
+                                    10826120, 
+                                    94514280, 
+                                    10289712, 
+                                      972693, 
+                                   172575973, 
+                                    14991931, 
+                                   212277542, 
+                                    16079266, 
+                                  -140109396, 
+                                   140007889, 
+                                    21190119, 
+                                    15178378, 
+                                   194851766, 
+                                   198398104, 
+                                    41137674, 
+                                    99761141, 
+                                       35857, 
+                                   162975824, 
+                                   136082162, 
+                                     7732095, 
+                                    -4889126 
+                                ];
+                maxThinkTime = 0
+                errorProbability = 0.25
+            }
+        }
+    }
+}
