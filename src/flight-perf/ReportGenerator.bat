6337eeae8cb9d681c17a6a74ff9352ae324633be
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2010-07-14 14:53:29 +0000

    Refreshing the flight perf instance configuration files - delete

--- src/flight-perf/ReportGenerator.bat
+++ src/flight-perf/ReportGenerator.bat
@@ -3,10 +3,10 @@ SETLOCAL
 CALL env.bat set
 CALL groovy src\org\tripplanner\flight\perf\report_generator\ConfigInstanceGraphReport.groovy %*
 CALL groovy src\org\tripplanner\flight\perf\report_generator\MonReport.groovy %*
-GOTO end
 CALL groovy src\org\tripplanner\flight\perf\report_generator\XMLSizeReport.groovy %*
 CALL groovy src\org\tripplanner\flight\perf\report_generator\RequestTypeReport.groovy %*
 CALL groovy src\org\tripplanner\flight\perf\report_generator\CacheReport.groovy %*
 CALL groovy src\org\tripplanner\flight\perf\report_generator\LogLevelReport.groovy %*
 ENDLOCAL
+GOTO end
 :end
