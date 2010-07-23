c3d0ec71dd7d3639620afd5bca107b5599df65ac
Miguel Pardal <1102072+miguelpardal@users.noreply.github.com>
2010-07-23 19:38:03 +0000

    Added concurrent users plot!

--- src/flight-perf/etc/plot/Users.gp
+++ src/flight-perf/etc/plot/Users.gp
@@ -2,7 +2,7 @@
 reset
 
 # name
-name = "Cache"
+name = "Users"
 
 # set style
 set style line 1 lc rgb 'grey20'
@@ -13,23 +13,25 @@ set style line 5 lc rgb 'grey60'
 set style line 6 lc rgb 'grey70'
 set style increment user
 
-set style data histogram
-set style histogram rowstacked
 set style fill pattern 1 border -1
+set style histogram rowstacked
+set style data histogram
 set boxwidth 0.75 relative
 
 # decoration
 set grid
 unset title
 # set xtic rotate by -30 scale 0
+set xlabel "Number of concurrent users"
 set ylabel "Average request processing time (ms)"
 set key outside right
 
 # define function to convert negative numbers to zero (elapsed time cannot be negative)
 zeronegative( a ) = ( a < 0 ) ? 0 : a
 
+
 # plot
-plot name . ".dat" using ($7):xtic(1) title "Hibernate Reads", '' using ($8) title "Hibernate Writes", '' using (zeronegative($6-($7+$8))) title "Hibernate Engine", '' using (zeronegative($5-$6)) title "Service", '' u (zeronegative($3-$5)) t "Web Service", '' u (zeronegative($2-$3)) t "Web"
+plot name . ".dat" using ($7):xtic(1) title "Hibernate Reads", '' using ($8) title "Hibernate Writes", '' using (zeronegative($6-($7+$8))) title "Hibernate Engine", '' using ($5-$6) title "Service", '' u ($3-$5) t "Web Service", '' u ($2-$3) t "Web"
 
 # text output
 set table name . ".table"
---
+++ src/flight-perf/src/org/tripplanner/flight/perf/report_generator/UsersReport.groovy
@@ -0,0 +1,147 @@
+package org.tripplanner.flight.perf.report_generator;
+
+import org.supercsv.io.*;
+import org.supercsv.prefs.*;
+
+import step.groovy.Helper;
+import org.tripplanner.flight.perf.helper.*;
+
+def reportId = "Users"
+def purpose = "compare simultaneous users"
+
+// command line options --------------------------------------------------------
+def cli = new CliBuilder(usage: "UsersReport")
+cli.h(longOpt: "help", required: false, args: 0, "Print this message")
+cli.cfg(longOpt: "config", required: false, args: 1, "Specify master configuration file")
+
+def options = cli.parse(args)
+assert (options)
+
+if (options.help) {
+    cli.usage();
+    return;
+}
+
+// load initial configuration --------------------------------------------------
+def configPath = "etc/config/Config.groovy";
+if (options.cfg) configPath = options.cfg;
+
+def config = Helper.parseConfig(configPath);
+assert config.perf.flight : "Expecting flight performance configuration file"
+Helper.configStringToFile(config);
+
+// main ------------------------------------------------------------------------
+println "Generating " + reportId + " report"
+println "to " + purpose
+
+// stats base dir
+def statsBaseDir = config.perf.flight.stats.outputBaseDir;
+assert statsBaseDir.exists() && statsBaseDir.isDirectory()
+
+// plot files dir
+def plotDir = config.perf.flight.report.plotDir;
+assert plotDir.exists() && plotDir.isDirectory()
+
+def plotFile = new File(plotDir, reportId + ".gp")
+assert plotFile.exists()
+
+// report output dir
+def outputDir = config.perf.flight.report.outputDir;
+if (!outputDir.exists()) outputDir.mkDir();
+assert outputDir.exists() && outputDir.isDirectory()
+
+// temporary directory
+def tempDir = File.createTempFile("report", "");
+tempDir.delete();
+tempDir.mkdir();
+assert tempDir.exists()
+
+
+// collect data ----------------------------------------------------------------
+
+def dirNameList = [
+    "SizeMedium",
+    "Users2",
+    "Users4",
+    "Users8",
+    "Users16",
+]
+
+// create map of stats files
+def statsFileMap = [ : ];
+dirNameList.each { dirName ->
+    def dir = new File(statsBaseDir, dirName);
+    if (!dir.exists()) {
+        println "WARNING: " + dirName + " not found."
+    } else {
+        def file = new File(dir, config.perf.flight.stats.overallFileName);
+        assert file.exists()
+        statsFileMap[dirName] = file;
+    }
+}
+
+// create data file
+def dataFile = new File(tempDir, reportId + ".dat");
+def o = new PrintStream(dataFile);
+
+def overallStatisticsHeaderList = CSVHelper.getOverallStatisticsHeaderList();
+def overallStatisticsHeaderArray = overallStatisticsHeaderList as String[];
+
+// print data file header
+o.printf("# 1-desc");
+final def dataHeaderList = [
+    "filter_time-mean",
+    "soap_time-mean",
+    "wsi_time-mean",
+    "si_time-mean",
+    "hibernate_time-mean",
+    "hibernate_read_time-mean",
+    "hibernate_write_time-mean"
+];
+for (int i = 0; i < dataHeaderList.size(); i++) {
+    o.printf(" %d-%s", i+2, dataHeaderList.get(i));
+}
+o.printf("%n");
+
+
+// print data file records
+def descMap = [
+    "SizeMedium" : "1",
+    "Users2"     : "2",
+    "Users4"     : "4",
+    "Users8"     : "8",
+    "Users16"    : "16",
+];
+descMap.each { key, value ->
+    descMap[key] = "\"" + descMap[key] + "\"";
+}
+
+dirNameList.each { dirName ->
+    def file = statsFileMap[dirName];
+    if (!file) return;
+
+    CsvMapReader csvMR = new CsvMapReader(new FileReader(file), CsvPreference.STANDARD_PREFERENCE);
+    // ignore headers in 1st line
+    csvMR.read(overallStatisticsHeaderArray);
+    // read data
+    def statsMap = csvMR.read(overallStatisticsHeaderArray);
+    assert statsMap
+
+    o.printf("%s", descMap[dirName]);
+    dataHeaderList.each { dataHeader ->
+        o.printf(" %s", statsMap[dataHeader]);
+    }
+    o.printf("%n");
+
+}
+o.close();
+
+
+// invoke gnuplot --------------------------------------------------------------
+
+ReportHelper.execGnuplot(reportId, tempDir, plotFile, outputDir);
+
+
+// -----------------------------------------------------------------------------
+println "Report " + reportId + " done!"
+println ""
